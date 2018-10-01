631120a9848aec92a47832b20e1065fe08ee9f2b
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-01 17:21:24 +0300

    db global olarak tanımlandı.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #else
 #endif
 
+#include <QQmlContext>
+
 #include "qmlmongodb.h"
 #include "../url.h"
 
@@ -16,7 +18,13 @@ int main(int argc, char *argv[])
 
     QMLMongoDB::instance(url,dbname);
 
+
+
     QQmlApplicationEngine engine;
+
+
+    engine.rootContext()->setContextProperty("db",new QMLMongoDB());
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
