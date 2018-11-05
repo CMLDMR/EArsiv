3acb0db9f96c8512bd2587b4a4f6e15753870ebc
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-05 10:17:44 +0300

    OCR implementasyonu eklendi.

--- cpp/utility.cpp
+++ cpp/utility.cpp
@@ -1,6 +1,56 @@
 #include "utility.h"
 
-Utility::Utility(QObject *parent) : QObject(parent)
+#include <QDebug>
+
+Utility::Utility(QObject *parent) : QObject(parent) , api(new tesseract::TessBaseAPI())
+{
+    if( api->Init(nullptr,"tur") ){
+        initFalse = false;
+    }else{
+        initFalse = true;
+    }
+}
+
+Utility::~Utility()
+{
+    api->End();
+
+    delete api;
+}
+
+QString Utility::runTesseract(QString filename)
 {
 
+
+    if( !initFalse ){
+        this->mExtractedText = "Failed OCR!";
+        emit this->extractedTextChanged();
+    }
+
+    QFileInfo fileinfo(filename);
+
+    qDebug() << fileinfo.suffix() ;
+    qDebug() << fileinfo.absoluteFilePath();
+
+
+
+
+    Pix* img = pixRead(filename.toStdString().c_str());
+
+    api->SetImage(img);
+
+    QString str = api->GetUTF8Text();
+
+
+    pixDestroy(&img);
+
+    this->mExtractedText = str;
+    emit this->extractedTextChanged();
+    return str;
+
+}
+
+QString Utility::extractedText() const
+{
+    return mExtractedText;
 }
--- cpp/utility.h
+++ cpp/utility.h
@@ -3,16 +3,44 @@
 
 #include <QtCore/QObject>
 #include <QtCore/qglobal.h>
+#include <QProcess>
+#include <QFile>
+#include <QFileInfo>
+
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
 
 class Utility : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString extractedText READ extractedText NOTIFY extractedTextChanged)
 public:
     explicit Utility(QObject *parent = nullptr);
 
+    ~Utility();
+
+
+    Q_INVOKABLE QString runTesseract(QString filename);
+
+
+    QString extractedText() const;
+
 signals:
+    void extractedTextChanged();
 
 public slots:
+
+private:
+    tesseract::TessBaseAPI *api;
+
+    bool initFalse;
+
+
+    QString mExtractedText;
+
 };
 
 #endif // UTILITY_H
